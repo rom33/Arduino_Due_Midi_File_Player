e3b4bba4fce26ba047a803a93c4c85a53bec6aa5
rom33 <49639367+rom33@users.noreply.github.com>
2020-11-18 22:15:57 +0100

    Add files via upload

--- midi_player.ino
+++ midi_player.ino
@@ -19,17 +19,13 @@ Adafruit_VS1053_FilePlayer musicPlayer = Adafruit_VS1053_FilePlayer(52, 46, 48, 
 #include <Adafruit_GFX.h>
 #include <MCUFRIEND_kbv.h>
 MCUFRIEND_kbv tft;
-Adafruit_GFX_Button mybutton = Adafruit_GFX_Button();
+Adafruit_GFX_Button button[2] = Adafruit_GFX_Button();
 
 // *** touch
 #define YP A2  // must be an analog pin, use "An" notation!
 #define XM A3  // must be an analog pin, use "An" notation!
 #define YM 8   // can be a digital pin
 #define XP 9   // can be a digital pin
-
-#define GetTouchPoints tp=myTouch.getPoint();xx=map(tp.x,TS_MINX,TS_MAXX,480,0);yy=map(tp.y,TS_MINY,TS_MAXY,320,0);
-#define ARRAY_SIZE(a) (sizeof(a)/sizeof(a[0]))
-
 int TS_MINX = 890;
 int TS_MAXX = 90;
 int TS_MINY = 890;
@@ -37,6 +33,7 @@ int TS_MAXY = 214;
 #define MINPRESSURE 25
 TouchScreen_kbv myTouch = TouchScreen_kbv(XP, YP, XM, YM, 300);
 TSPoint_kbv tp;
+#define GetTouchPoints tp=myTouch.getPoint();xx=map(tp.x,TS_MINX,TS_MAXX,320,0);yy=map(tp.y,TS_MINY,TS_MAXY,240,0);
 
 unsigned short xx, yy;
 String filename;
@@ -44,12 +41,17 @@ String names[500] = {};
 int count;
 File root, entry;
 int i;
+
 void setup() {
   Serial.begin(9600);
   tft.begin(tft.readID());
   tft.setRotation(1);
   tft.setTextColor(TFT_WHITE, TFT_BLUE);
   tft.fillScreen(TFT_BLUE);
+  button[0].initButton(&tft, 180, 200, 100, 40, TFT_WHITE, TFT_CYAN, TFT_BLACK, "prev", 2);
+  button[1].initButton(&tft, 300, 200, 100, 40, TFT_WHITE, TFT_CYAN, TFT_BLACK, "next", 2);
+  button[0].drawButton(false);
+  button[1].drawButton(false);
   // initialise the music player
   musicPlayer.begin();
   musicPlayer.sciWrite(VS1053_REG_WRAMADDR, VS1053_GPIO_DDR);
@@ -66,14 +68,29 @@ void setup() {
 void loop() {
   //  for (uint8_t i = 0; i < ARRAY_SIZE(names); i++)
   Serial.println(names[i]);
+  Serial.println(i);
   musicPlayer.startPlayingFile(names[i].c_str());
   while (musicPlayer.playingMusic) {
-    GetTouchPoints
+    GetTouchPoints;
     if (tp.z > MINPRESSURE) {
+      GetTouchPoints;
+      if (button[0].contains(xx, yy)); {
+        Serial.println("prev");
+        i -= 1;
+        if (i < 0) i = count - 1;
+        musicPlayer.stopPlaying();
+        delay(500);
+        break;
+      }
+      if (button[1].contains(xx, yy)); {
+        Serial.println("next");
+        i += 1;
+        if (i >= count) i = 0;
+        musicPlayer.stopPlaying();
+        delay(500);
+        break;
+      }
       delay(200);
-      i += 1;
-      if (i > ARRAY_SIZE(names)) i = 0;
-      musicPlayer.stopPlaying();
     }
   }
 }
